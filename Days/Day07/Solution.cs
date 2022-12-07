983e269cb96b33fb41135c9b4c020722cb3c592b
Vaedelund, Tomas (Växjö) SWE <tomas.vaedelund@heidelbergcement.com>
2022-12-07 14:10:22 +0100

    day07 improved

--- Days/Day07/Solution.cs
+++ Days/Day07/Solution.cs
@@ -5,69 +5,46 @@ namespace AdventOfCode2022.Days.Day07;
 
 public class Solution : ISolver
 {
-    // < 18929272 (to high)
-    // > 1680 (to low)
-    // > 3487907 (to low)
-
     Dictionary<string, int> fileSystem = new();
 
     public object PartOne(string input) => GetFileSystem(input)
         .Where(x => x.Value <= 100000)
         .Sum(x => x.Value);
 
-    public object PartTwo(string input) => GetFileSystem(input)
-        .Where(x => x.Value >= (GetFileSystem(input).Max(x => x.Value) - 40000000))
-        .OrderBy(x => x.Value)
-        .First().Value;
+    public object PartTwo(string input)
+    {
+        var max = GetFileSystem(input).Max(x => x.Value);
+        return GetFileSystem(input)
+            .Where(x => x.Value >= max - 40000000)
+            .OrderBy(x => x.Value)
+            .First().Value;
+    }
 
     Dictionary<string, int> GetFileSystem(string input)
     {
-        if (!fileSystem.Any())
-        {
-            var currentPath = "/";
+        var fileSystem = new Dictionary<string, int>();
+        var currentPath = new Stack<string>();
 
-            foreach (var row in input.Split("\r\n").Skip(1))
+        foreach (var row in input.Split("\r\n"))
+        {
+            if (row == "$ cd ..")
             {
-                var test = row.Split(' ');
-                if (test is [_, _, var path])
+                _ = currentPath.Pop();
+            }
+            else if (row.StartsWith("$ cd"))
+            {
+                currentPath.Push(string.Join("", currentPath) + row.Split(' ')[2]);
+            }
+            else if (!row.StartsWith("$") && !row.StartsWith("dir"))
+            {
+                var size = int.Parse(row.Split(' ')[0]);
+                foreach (var subpath in currentPath)
                 {
-                    if (path == "..")
-                    {
-                        currentPath = string.Join("/", currentPath.Split('/').SkipLast(1));
-                    }
-                    else
-                    {
-                        currentPath = $"{currentPath}/{path}";
-                    }
-                }
-                else
-                {
-                    if (test[0] is not "dir" and not "$")
-                    {
-                        AddSizes(fileSystem, currentPath, int.Parse(test[0]));
-                    }
+                    fileSystem[subpath] = fileSystem.GetValueOrDefault(subpath) + size;
                 }
             }
         }
 
         return fileSystem;
-    }
-
-    void AddSizes(Dictionary<string, int> structure, string path, int size)
-    {
-        var pathSplit = path.Split('/').Skip(1).ToArray();
-        var pathItem = string.Empty;
-        for (int i = 0; i < pathSplit.Length; i++)
-        {
-            pathItem += $"/{string.Join("/", pathSplit.Skip(i).Take(1))}";
-            if (structure.ContainsKey(pathItem))
-            {
-                structure[pathItem] += size;
-            }
-            else
-            {
-                structure[pathItem] = size;
-            }
-        }
     }
 }
