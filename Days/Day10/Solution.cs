36d5f23599aed1801456c7d5126cf9d003967b41
Tomas Vaedelund <tvaedelund@gmail.com>
2022-12-10 08:32:48 +0100

    day10 part 2

--- Days/Day10/Solution.cs
+++ Days/Day10/Solution.cs
@@ -5,27 +5,55 @@ namespace AdventOfCode2022.Days.Day10;
 
 public class Solution : ISolver
 {
-    public object PartOne(string input) => GetSignals(input).Where(x => x.tick is 20 or 60 or 100 or 140 or 180 or 220).Select(x => x.tick * x.X).Sum();
+    public object PartOne(string input) => GetSignals(input).Where(x => x.cycle is 20 or 60 or 100 or 140 or 180 or 220).Select(x => x.cycle * x.X).Sum();
 
-    public object PartTwo(string input) => 0;
+    public object PartTwo(string input)
+    {
+        var a = GetSignals(input);
+        PrintScreen(a);
+        return 0;
+    }
 
-    private IEnumerable<(int X, int tick)> GetSignals(string input)
+    record Cycle(int X, int cycle, char pixel);
+
+    private IEnumerable<Cycle> GetSignals(string input)
     {
         var X = 1;
-        var tick = 1;
+        var cycle = 1;
         foreach (var line in input.Split("\r\n"))
         {
             var lineSplit = line.Split(' ');
             if (lineSplit.Length == 1)
             {
-                yield return (X, tick++);
+                yield return new(X, cycle++, DrawPixel(X, cycle));
             }
             else
             {
-                yield return (X, tick++);
-                yield return (X, tick++);
+                yield return new(X, cycle++, DrawPixel(X, cycle));
+                yield return new(X, cycle++, DrawPixel(X, cycle));
                 X += int.Parse(lineSplit[1]);
             }
+        }
+    }
+
+    private char DrawPixel(int X, int pos)
+    {
+        pos -= 2;
+        pos %= 40;
+        return (pos >= (X - 1) && pos <= (X + 1)) ? '#' : '.';
+    }
+
+    private void PrintScreen(IEnumerable<Cycle> cycles)
+    {
+        var rows = cycles.Chunk(40);
+        foreach (var row in rows)
+        {
+            for (int i = 0; i < row.Length; i++)
+            {
+                Console.Write(row[i].pixel);
+            }
+
+            Console.WriteLine();
         }
     }
 }
