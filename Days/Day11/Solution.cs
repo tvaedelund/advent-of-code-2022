546808447bea8b124ca040d5dc641d3687cdcc1a
Tomas Vaedelund <tvaedelund@gmail.com>
2022-12-11 11:01:24 +0100

    day11 part 2 (improved)

--- Days/Day11/Solution.cs
+++ Days/Day11/Solution.cs
@@ -1,5 +1,3 @@
-using System.Text;
-using System.Text.RegularExpressions;
 using AdventOfCode.Helpers;
 
 namespace AdventOfCode2022.Days.Day11;
@@ -9,48 +7,56 @@ public class Solution : ISolver
     public object PartOne(string input)
     {
         var monkeys = Parse(input).ToList();
-        
+
         for (int i = 0; i < 20; i++)
         {
-            InspectItems(monkeys);
+            InspectItems(monkeys, worryLevel => worryLevel / 3);
         }
 
-        var topMonkeys = monkeys.Select(monkey => monkey.inspections.Sum()).OrderByDescending(sum => sum).Take(2).ToList();
+        var topMonkeys = monkeys.OrderByDescending(m => m.inspections).Take(2).ToList();
 
-        return topMonkeys[0] * topMonkeys[1];
+        return topMonkeys[0].inspections * topMonkeys[1].inspections;
     }
 
-    public object PartTwo(string input) => 0;
+    public object PartTwo(string input)
+    {
+        var monkeys = Parse(input).ToList();
 
-    record Monkey(Queue<long> items, int test, int ifTrue, int ifFalse, string operation, List<int> inspections);
+        // https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+        var mod = 1;
+        foreach (var m in monkeys)
+        {
+            mod *= m.test;
+        }
 
-    void InspectItems(List<Monkey> monkeys)
+        for (int i = 0; i < 10000; i++)
+        {
+            InspectItems(monkeys, worryLevel => worryLevel % mod);
+        }
+
+        var topMonkeys = monkeys.OrderByDescending(m => m.inspections).Take(2).ToList();
+
+        return (long)topMonkeys[0].inspections * (long)topMonkeys[1].inspections;
+    }
+
+
+    record Monkey(Queue<long> items, int test, int ifTrue, int ifFalse, Func<long, long> operation)
+    {
+        public int inspections { get; set; }
+    }
+
+    void InspectItems(List<Monkey> monkeys, Func<long, long> calcWorryLevel)
     {
         foreach (var monkey in monkeys)
         {
             while (monkey.items.Any())
             {
                 var item = monkey.items.Dequeue();
-                var newItem = DoMonkeyBusiness(item, monkey.operation) / 3;
+                var newItem = calcWorryLevel(monkey.operation(item));
                 monkeys[newItem % monkey.test == 0 ? monkey.ifTrue : monkey.ifFalse].items.Enqueue(newItem);
-                monkey.inspections.Add(1);
+                monkey.inspections++;
             }
         }
-    }
-
-    long DoMonkeyBusiness(long old, string operation)
-    {
-        var opSplit = operation.Split(' ');
-        var end = opSplit[2] == "old" ? old : long.Parse(opSplit[2]);
-
-        return opSplit[1] switch
-        {
-            "+" => old + end,
-            "-" => old - end,
-            "*" => old * end,
-            "/" => old / end,
-            _ => old
-        };
     }
 
     IEnumerable<Monkey> Parse(string input)
@@ -59,40 +65,30 @@ public class Solution : ISolver
 
         foreach (var monkey in monkeys)
         {
-            var monkeyRows = monkey.Split("\r\n");
-            var startingItems = new Queue<long>();
-            var operation = string.Empty;
-            var test = 0;
-            var ifTrue = 0;
-            var ifFalse = 0;
-            foreach (var row in monkeyRows)
+            var instructions = monkey.Split("\r\n");
+            var startingItems = new Queue<long>(instructions[1].Split("Starting items: ")[1].Split(", ").Select(long.Parse));
+
+            Func<long, long> operation;
+            if (instructions[2].Contains("Operation: new = old * old"))
             {
-                if (row.Contains("Starting items:"))
-                {
-                    foreach (var item in row.Split(": ")[1].Split(", ").Select(int.Parse).ToList())
-                    {
-                        startingItems.Enqueue(item);
-                    }
-                }
-                else if (row.Contains("Operation:"))
-                {
-                    operation = row.Replace("  Operation: new = ", "");
-                }
-                else if (row.Contains("Test:"))
-                {
-                    test = int.Parse(row.Replace("  Test: divisible by ", ""));
-                }
-                else if (row.Contains("If true:"))
-                {
-                    ifTrue = int.Parse(row.Last().ToString());
-                }
-                else if (row.Contains("If false:"))
-                {
-                    ifFalse = int.Parse(row.Last().ToString());
-                }
+                operation = (long old) => old * old;
+            }
+            else if (instructions[2].Contains("Operation: new = old * "))
+            {
+                var end = int.Parse(instructions[2].Split("Operation: new = old * ")[1]);
+                operation = (long old) => old * end;
+            }
+            else
+            {
+                var end = int.Parse(instructions[2].Split("Operation: new = old + ")[1]);
+                operation = (long old) => old + end;
             }
 
-            yield return new Monkey(startingItems, test, ifTrue, ifFalse, operation, new());
+            var test = int.Parse(instructions[3].Split("Test: divisible by ")[1]);
+            var ifTrue = int.Parse(instructions[4].Split("If true: throw to monkey ")[1]);
+            var ifFalse = int.Parse(instructions[5].Split("If false: throw to monkey ")[1]);
+
+            yield return new Monkey(startingItems, test, ifTrue, ifFalse, operation);
         }
     }
 }
