95762c685af3bece0ae3845b701004e88caa1a40
Vaedelund, Tomas (Växjö) SWE <tomas.vaedelund@heidelbergcement.com>
2022-12-16 07:57:13 +0100

    day16 prepared

---
+++ Days/Day16/Solution.cs
@@ -0,0 +1,11 @@
+using System.Text.RegularExpressions;
+using AdventOfCode.Helpers;
+
+namespace AdventOfCode2022.Days.Day16;
+
+public class Solution : ISolver
+{
+    public object PartOne(string input) => 0;
+
+    public object PartTwo(string input) => 0;
+}
