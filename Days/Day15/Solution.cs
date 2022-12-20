69ab33bf32744fd888794f5b5af0862b71d57b46
Vaedelund, Tomas (Växjö) SWE <tomas.vaedelund@heidelbergcement.com>
2022-12-20 11:05:18 +0100

    day15 part 1 reorg

--- Days/Day15/Solution.cs
+++ Days/Day15/Solution.cs
@@ -5,17 +5,21 @@ namespace AdventOfCode2022.Days.Day15;
 
 public class Solution : ISolver
 {
-    public object PartOne(string input) => Solve(input, 2000000);
-    public object PartTwo(string input) => 0;
-
-    int Solve(string input, int yLevel)
+    public object PartOne(string input)
     {
-        var readings = from line in input.Split("\r\n")
-                       let groups = Regex.Match(line, @"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)").Groups
-                       let reading = new Reading(new Pos(int.Parse(groups[1].Value), int.Parse(groups[2].Value)), new Pos(int.Parse(groups[3].Value), int.Parse(groups[4].Value)), yLevel)
-                       select reading;
+        var readings = Parse(input, 2000000);
 
         return Math.Abs(readings.Min(r => r.minX)) + Math.Abs(readings.Max(r => r.maxX));
+    }
+
+    public object PartTwo(string input) => 0;
+
+    IEnumerable<Reading> Parse(string input, int yLevel)
+    {
+        return from line in input.Split("\r\n")
+               let groups = Regex.Match(line, @"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)").Groups
+               let reading = new Reading(new Pos(int.Parse(groups[1].Value), int.Parse(groups[2].Value)), new Pos(int.Parse(groups[3].Value), int.Parse(groups[4].Value)), yLevel)
+               select reading;
     }
 
     record Pos(int x, int y);
